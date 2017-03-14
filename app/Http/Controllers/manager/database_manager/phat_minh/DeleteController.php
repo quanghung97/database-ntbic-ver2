b898dabba467828d5a6b5a7ce02ae27fddb586dc
truongnhung <truongnhung367@gmail.com>
2017-03-14 22:57:37 +0700

    Update DeleteController.php
    a

--- app/Http/Controllers/manager/database_manager/phat_minh/DeleteController.php
+++ app/Http/Controllers/manager/database_manager/phat_minh/DeleteController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\manager\database_manager\phat_minh;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
+use Illuminate\Support\Facades\Redirect;
+use App\bang_phat_minh_sang_che;
+use Illuminate\Support\Facades\Input;
+use Laracasts\Flash\FlashServiceProvider;
+
 
 class DeleteController extends Controller
 {
-    //
+    public function index(Request $request){
+     	$id = $request->id;
+    	$rel=bang_phat_minh_sang_che::find($id);
+    	$rel->delete();
+    	return Redirect::to('quan-tri-vien/quan-ly-du-lieu/phat-minh');
+    }
 }
