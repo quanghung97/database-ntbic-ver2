f22d389ff426dd9a271b9345463226f8a6c9a831
truongnhung <truongnhung367@gmail.com>
2017-03-14 22:58:57 +0700

    Update HomeController.php
    a

--- app/Http/Controllers/manager/database_manager/phat_minh/HomeController.php
+++ app/Http/Controllers/manager/database_manager/phat_minh/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\manager\database_manager\phat_minh;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\bang_phat_minh_sang_che;
 
 class HomeController extends Controller
 {
     public function index()
     {
-    	return view('database_manager.phat_minh.index');
+    	$pm=bang_phat_minh_sang_che::paginate(10);
+    	return view('database_manager.phat_minh.index')->with(['phat_minh'=>$pm]);
     }
 }
+
