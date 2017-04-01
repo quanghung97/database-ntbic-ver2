d41b9112e99c7b2f216422a22bc102d34dc4cb6a
truongnhung <truongnhung367@gmail.com>
2017-04-01 19:12:42 +0700

    add js share

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,7 @@
   	<!-- style -->
   	<link rel="stylesheet" type="text/css" href="{{ URL::asset('public/css/master.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('public/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+	@yield('javascript')
 	<!-- end style -->
 </head>
 <body>
@@ -134,6 +135,13 @@ function inbaiviet1() {
     window.print();
 }
 </script>
+<script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.8";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 	@yield('script')
 </body>
 </html>
